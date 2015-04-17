70505510729074959ae858bd2f3ff3ee26d10ebe
Rob Kaufman <rob@notch8.com>
2015-04-17 00:15:16 -0700

    layout adjustments

--- app/models/need.rb
+++ app/models/need.rb
@@ -6,6 +6,8 @@ class Need < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
+  validates :amount_requested, numericality: { greater_than: 0 }
+
   def percent_complete
     (self.amount_donated / self.amount_requested.to_f) * 100
   end
