cb9310e9368cf657cfa9b687cb77f07ff583d0b3
Rob Kaufman <rob@notch8.com>
2015-05-17 13:42:33 -0700

    add wysig to need new, make donation amounts in to a helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,12 @@ module ApplicationHelper
     Organization.first
   end
 
+  def amount_list(additional_amount = 5)
+    ([[number_to_currency(5, :precision => 0), 5]] +
+    (1..50).collect { |i| ["#{number_to_currency(25*i, :precision => 0)}", 25*i]} +
+    [[number_to_currency(additional_amount, :precision => 0), additional_amount]]).uniq
+  end
+
   def bootstrap_class_for flash_type
     case flash_type.to_s
     when 'success'
