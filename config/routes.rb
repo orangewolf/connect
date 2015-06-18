43ed76c464370128f5a5902ec0d3b6a193001ec6
Rob Kaufman <rob@notch8.com>
2015-06-18 06:39:10 -0700

    user edit function

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   resources :needs
 
+  resources :users
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   root 'needs#show', :id => 1
---
+++ spec/views/users/new.html.erb_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "users/new", type: :view do
+  before(:each) do
+    assign(:user, User.new(
+      :name => "MyString",
+      :image => "",
+      :email => "MyString",
+      :password => "MyString",
+      :password_confirmation => "MyString"
+    ))
+  end
+
+  it "renders new user form" do
+    render
+
+    assert_select "form[action=?][method=?]", users_path, "post" do
+
+      assert_select "input#user_name[name=?]", "user[name]"
+
+      assert_select "input#user_image[name=?]", "user[image]"
+
+      assert_select "input#user_email[name=?]", "user[email]"
+
+      assert_select "input#user_password[name=?]", "user[password]"
+
+      assert_select "input#user_password_confirmation[name=?]", "user[password_confirmation]"
+    end
+  end
+end
