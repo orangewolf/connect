ca87c80b1bd94474be36dae07ee6534d4cb7681d
Rob Kaufman <rob@notch8.com>
2015-08-03 00:02:39 -0700

    carrierwave in dev

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,24 +5,16 @@ CarrierWave.configure do |config|
     config.enable_processing = false
     config.root = "#{Rails.root}/tmp"
   else
+    config.fog_provider = 'fog/aws'                        # required
     config.fog_credentials = {
-      # Configuration for Amazon S3 should be made available through an Environment variable.
-      # For local installations, export the env variable through the shell OR
-      # if using Passenger, set an Apache environment variable.
-      #
-      # In Heroku, follow http://devcenter.heroku.com/articles/config-vars
-      #
-      # $ heroku config:add S3_KEY=your_s3_access_key S3_SECRET=your_s3_secret S3_REGION=eu-west-1 S3_ASSET_URL=http://assets.example.com/ S3_BUCKET_NAME=s3_bucket/folder
-
       # Configuration for Amazon S3
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['SECRET_KEY_BASE'],
-      :aws_secret_access_key => ENV['SECRET_ACCESS_KEY'],
-      :region                => 'us-east-1'
+      :aws_secret_access_key => ENV['SECRET_ACCESS_KEY']
     }
     config.storage = :fog
     config.fog_directory    = ENV['S3_BUCKET_NAME']
-    config.fog_host         = "s3.amazonaws.com/#{ENV['S3_BUCKET_NAME']}"
+    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
   end
 
   config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
