b9ca0bcc5eaf3752d1147b1c5fc8efa03a4a26af
Vincent Colavin <vcolavin@gmail.com>
2015-10-20 14:49:33 -0700

    add and pass tests for users managing needs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let (:organization) { Organization.create title: "test", access_code: "test" }
+
   context "access code" do
-    let (:organization) { Organization.create title: "test", access_code: "test" }
     let (:user) { User.new email: "test@test.test", password: "testing123" }
 
     it "should be valid with an organization access code" do
@@ -27,6 +28,32 @@ RSpec.describe User, type: :model do
       user.update_attributes({email: "new@new.new", access_code: "chumbawumba"})
       expect(user).to be_valid
     end
+  end
 
+  context "roles and abilities" do
+    let (:admin_user) do 
+      user = User.create(email: "admin@test.com", password: "testing123", access_code: "test")
+      user.add_role("admin")
+      user
+    end
+    let (:non_admin_user) { User.create(email: "bob@test.com", password: "testing123", access_code: "test") }
+
+    let (:need_1) { Need.create(title: "testneed1", user_id: non_admin_user.id) }
+    let (:need_2) { Need.create(title: "testneed2", user_id: 0) }
+
+    it "admin should be able to edit needs" do
+      ability = Ability.new(admin_user)
+      expect(ability).to be_able_to(:edit, need_1)
+    end
+
+    it "non-admin should be able to edit it's own needs" do
+      ability = Ability.new(non_admin_user)
+      expect(ability).to be_able_to(:edit, need_1)
+    end
+
+    it "non-admin should not be able to edit others' needs" do
+      ability = Ability.new(non_admin_user)
+      expect(ability).not_to be_able_to(:edit, need_2)    
+    end
   end
 end
